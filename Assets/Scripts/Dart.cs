a9b5a525019a2814e2ba4844bfa1976cf760d076
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2020-05-09 19:59:57 +0200

    Starting scripting

---
+++ Assets/Scripts/Dart.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dart : MonoBehaviour
+{
+    Collider dart_point;
+    Rigidbody rigid_body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dart_point = GetComponent<BoxCollider>();
+        rigid_body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        rigid_body.constraints = RigidbodyConstraints.FreezeRotation;
+        rigid_body.constraints = RigidbodyConstraints.FreezePosition;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+class Player
+{
+    int points = 0;
+    int darts_thrown = 0;
+}
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject red_Dart;
+    public GameObject blue_Dart;
+
+    // Internal variables
+    List<GameObject> current_darts;
+
+    public int darts_thrown = 0;
+
+    [HideInInspector]
+    List<Player> players;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NewRound(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void NewRound(int player_index)
+    {
+        current_darts.Clear();
+
+        for (int i = 0; i < 3; ++i)
+        {
+            if (player_index == 0)
+            {
+                current_darts.Add(Instantiate(red_Dart));
+            }
+            else
+            {
+                current_darts.Add(Instantiate(blue_Dart));
+            }
+        }
+    }
+}
