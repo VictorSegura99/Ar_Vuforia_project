6789f99257937259b1c8b0848c1effc98c67f35e
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2020-05-10 14:07:42 +0200

    No aconsegueixo fer el lerp i he de anar a dinar F

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Numerics;
 using UnityEngine;
 
 class Player
 {
     int points = 0;
-    int darts_thrown = 0;
 }
 
 public class GameManager : MonoBehaviour
@@ -15,30 +14,66 @@ public class GameManager : MonoBehaviour
     public GameObject red_Dart;
     public GameObject blue_Dart;
 
+    public float dart_transition_time = 0.5f;
+
     // Internal variables
     List<GameObject> current_darts = new List<GameObject>();
+    bool dart_taken = false;
+    bool dart_in_position = false;
+    float time_at_lerp_start = 0.0f;
+    bool lerping_dart = false;
+    Transform original_dart_transform;
 
-    public int darts_thrown = 0;
-
-    [HideInInspector]
+    // Players
     List<Player> players = new List<Player>();
-    int current_player = 0;
+    Player current_player;
 
     // Start is called before the first frame update
     void Start()
     {
-        NewRound(0);
+        for (int i = 0; i < 2; ++i)
+        {
+            players.Add(new Player());
+        }
+        
+        NewRound(1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!dart_taken)
+            {
+                TakeDart();
+            }
+            else if (dart_taken && dart_in_position) 
+            {
+                ThrowDart();
+            }
+        }
+
+        if (lerping_dart)
+        {
+            float t = (Time.realtimeSinceStartup - time_at_lerp_start) / dart_transition_time;
+
+            Vector3.Lerp(original_dart_transform.localPosition, new Vector3(0, -0.45f, 1.25f), t);
+            Quaternion.Lerp(original_dart_transform.localRotation, Quaternion.Euler(0, 180, 0), t);
+
+            if (t >= 1)
+            {
+                current_darts.Last().transform.localPosition = new Vector3(0, -0.45f, 1.25f);
+                current_darts.Last().transform.localRotation = Quaternion.Euler(0, 180, 0);
+                dart_in_position = true;
+                lerping_dart = false;
+            }
+        }
     }
 
     void NewRound(int player_index)
     {
-        current_player = player_index;
+        current_player = players[player_index];
 
         if (current_darts.Count != 0)
         {
@@ -50,33 +85,56 @@ public class GameManager : MonoBehaviour
             if (player_index == 0)
             {
                 current_darts.Add(Instantiate(red_Dart, GameObject.Find("Darts").transform));
-                current_darts[i].transform.SetPositionAndRotation(
-                    new UnityEngine.Vector3(current_darts[i].transform.position.x + (i - 1) * 0.2f, 0, -0.45f),
-                     UnityEngine.Quaternion.Euler(-5, 0, 45));
+              
             }
             else
             {
                 current_darts.Add(Instantiate(blue_Dart, GameObject.Find("Darts").transform));
-                current_darts[i].transform.SetPositionAndRotation(
-                   new UnityEngine.Vector3(current_darts[i].transform.position.x + (i - 1) * 0.2f, 1, -5.33f),
-                   UnityEngine.Quaternion.Euler(-5, 0, 45));
             }
+
+            current_darts[i].transform.SetPositionAndRotation(
+                  new Vector3(current_darts[i].transform.position.x + (i - 1) * 0.2f, 0, -0.45f),
+                   Quaternion.Euler(-5, 0, 45));
         }
     }
 
     void TakeDart()
     {
-        Destroy(current_darts.Last());
-        current_darts.Remove(current_darts.Last());
-        GameObject dart;
+        dart_taken = true;
 
-        if (current_player == 0) 
-        {
-            dart = GameObject.Instantiate(red_Dart, GameObject.Find("ARCamera").transform);
-        }
-        else
-        {
+        current_darts.Last().transform.SetParent(GameObject.Find("ARCamera").transform);
+        original_dart_transform = current_darts.Last().transform;
 
-        }
+        time_at_lerp_start = Time.realtimeSinceStartup;
+
+        // Lerp to camera
+        //StartCoroutine("LerpToCamera");
+        lerping_dart = true;
     }
+
+    void ThrowDart()
+    {
+        dart_in_position = false;
+        current_darts.Last().GetComponent<Rigidbody>().AddForce(current_darts.Last().transform.forward * 50);
+        current_darts.Last().GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    //IEnumerator LerpToCamera()
+    //{
+    //    float t = (Time.realtimeSinceStartup - time_at_lerp_start) / dart_transition_time;
+
+    //    Vector3.Lerp(original_dart_transform.localPosition, new Vector3(0, -0.45f, 1.25f), t);
+    //    Quaternion.Lerp(original_dart_transform.localRotation, Quaternion.Euler(0, 180, 0), t);
+
+    //    if (t >= 1)
+    //    {
+    //        current_darts.Last().transform.localPosition = new Vector3(0, -0.45f, 1.25f);
+    //        current_darts.Last().transform.localRotation = Quaternion.Euler(0, 180, 0);
+    //        dart_in_position = true;
+    //        StopCoroutine("LerpToCamera");
+    //    }
+
+    //    Debug.Log("1 cicle");
+    //    yield return new WaitForFixedUpdate();
+    //}
 }
