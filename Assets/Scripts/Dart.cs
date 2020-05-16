88a328cb3373f42f5ed1fe1d18c08a844d1e3c3c
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2020-05-16 11:01:34 +0200

    throw dart

--- Assets/Scripts/Dart.cs
+++ Assets/Scripts/Dart.cs
@@ -2,24 +2,51 @@
 
 public class Dart : MonoBehaviour
 {
-    Collider dart_point;
-    Rigidbody rigid_body;
-    // Start is called before the first frame update
+    Vector2 startPos, endPos, direction; // touch start position, touch end position, swipe direction
+    float touchTimeStart, touchTimeFinish, timeInterval; // to calculate swipe time to sontrol throw force in Z direction
+
+    public float throwForceInXandY = 1f;
+    public float throwForceInZ = 50f;
+
+    Rigidbody rb;
+
     void Start()
     {
-        dart_point = GetComponent<BoxCollider>();
-        rigid_body = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        // if you touch the screen
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            // getting touch position and marking time when you touch the screen
+            touchTimeStart = Time.time;
+            startPos = Input.GetTouch(0).position;
+        }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        rigid_body.constraints = RigidbodyConstraints.FreezeRotation;
-        rigid_body.constraints = RigidbodyConstraints.FreezePosition;
+        // if you release your finger
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            // marking time when you release it
+            touchTimeFinish = Time.time;
+
+            // calculate swipe time interval 
+            timeInterval = touchTimeFinish - touchTimeStart;
+
+            // getting release finger position
+            endPos = Input.GetTouch(0).position;
+
+            // calculating swipe direction in 2D space
+            direction = startPos - endPos;
+
+            // add force to balls rigidbody in 3D space depending on swipe time, direction and throw forces
+            rb.isKinematic = false;
+            rb.AddForce(-direction.x * throwForceInXandY, -direction.y * throwForceInXandY, throwForceInZ / timeInterval);
+
+            // Destroy dart in 4 seconds
+            Destroy(gameObject, 3f);
+        }
     }
 }
