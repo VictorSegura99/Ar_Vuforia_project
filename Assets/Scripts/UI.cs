d061ed7c2150e0b91a5d1b09af8c8d08736e02d2
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2020-05-16 11:53:17 +0200

    UI function to update points smoothly

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    [HideInInspector]
+    public TextMeshPro points;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        points = gameObject.transform.GetChild(1).GetComponent<TextMeshPro>();
+        points.text = "0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateCurrentPlayerPoints(int new_points)
+    {
+        StartCoroutine(LerpPoints(int.Parse(this.points.text) + new_points, int.Parse(this.points.text)));
+    }
+    
+    IEnumerator LerpPoints(int current_points, int previous_points)
+    {
+        float time = Time.realtimeSinceStartup;
+
+        while (current_points != previous_points) 
+        {
+            points.text = Mathf.Lerp(previous_points, current_points, (time - Time.realtimeSinceStartup) / 0.5f).ToString();
+
+            if (((time - Time.realtimeSinceStartup) / 0.5f) >= 1)
+            {
+                current_points = previous_points;
+                points.text = current_points.ToString();
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
