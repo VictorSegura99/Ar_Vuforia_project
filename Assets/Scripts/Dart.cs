eec2d0530e361e38d820abae906270740a7ff2ea
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2020-05-20 00:34:52 +0200

    ads

--- Assets/Scripts/Dart.cs
+++ Assets/Scripts/Dart.cs
@@ -8,7 +8,7 @@ public class Dart : MonoBehaviour
     float touchTimeStart, touchTimeFinish, timeInterval; // to calculate swipe time to sontrol throw force in Z direction
 
     public float throwForceInXandY = 0f;
-    public float throwForceInZ = 1300f;
+    public float throwForceInZ = 1500f;
 
     Rigidbody rb;
 
@@ -62,7 +62,8 @@ public class Dart : MonoBehaviour
             // add force to balls rigidbody in 3D space depending on swipe time, direction and throw forces
             rb.useGravity = true;
             rb.isKinematic = false;
-            rb.AddForce(-direction.x * throwForceInXandY, -direction.y * throwForceInXandY, (throwForceInZ / timeInterval) * 10);
+            Physics.gravity = new Vector3(0,-300, 0);
+            rb.AddForce(-direction.x * 100, -direction.y * 25, (throwForceInZ / timeInterval) * 10);
 
             Destroy(gameObject, 3.0F);
             GameObject.Find("GameManager").GetComponent<GameManager>().Invoke("ActiveDart", 1.3F);
@@ -72,30 +73,30 @@ public class Dart : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        //if (other.gameObject.GetComponent<TriggerData>().absolute_trigger)
-        //{
-        //    rb.isKinematic = true;
-        //    GetComponent<AudioSource>().Play();
+        if (other.gameObject.GetComponent<TriggerData>().absolute_trigger)
+        {
+            rb.isKinematic = true;
+            GetComponent<AudioSource>().Play();
 
-        //    if (triggers_passed.Count != 0) 
-        //    {
-        //        TriggerData lowest_priority = triggers_passed.First();
+            if (triggers_passed.Count != 0)
+            {
+                TriggerData lowest_priority = triggers_passed.First();
 
-        //        for (int i = 0; i < triggers_passed.Count; ++i) 
-        //        {
-        //            if (triggers_passed[i].priority < lowest_priority.priority)
-        //            {
-        //                lowest_priority = triggers_passed[i];
-        //            }
-        //        }
+                for (int i = 0; i < triggers_passed.Count; ++i)
+                {
+                    if (triggers_passed[i].priority < lowest_priority.priority)
+                    {
+                        lowest_priority = triggers_passed[i];
+                    }
+                }
 
-        //        GameObject.Find("GameManager").GetComponent<GameManager>().current_player.current_round_points += lowest_priority.points;
-        //        GameObject.Find("Points").GetComponent<UI>().UpdateCurrentPlayerPoints(lowest_priority.points);
-        //    }
-        //}
-        //else
-        //{
-        //    triggers_passed.Add(other.gameObject.GetComponent<TriggerData>());
-        //}
+                GameObject.Find("GameManager").GetComponent<GameManager>().current_player.current_round_points += lowest_priority.points;
+                GameObject.Find("Points").GetComponent<UI>().UpdateCurrentPlayerPoints(lowest_priority.points);
+            }
+        }
+        else
+        {
+            triggers_passed.Add(other.gameObject.GetComponent<TriggerData>());
+        }
     }
 }
